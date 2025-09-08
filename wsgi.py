27f0b102b6085790d1aa003939a01be4dac909ed
Chris R <cr625@drexel.edu>
2025-09-08 07:26:26 -0400

    clean up scripts

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-from app import create_app
-
-# Ensure production config
-os.environ.setdefault('ENVIRONMENT', 'production')
-os.environ.setdefault('CONFIG_MODULE', 'config')
-
-# Gunicorn entry point
-app = create_app('config')
