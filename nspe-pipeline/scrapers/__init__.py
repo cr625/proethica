5141bdbd9e5c1659a86f66778da52a3a1e97a016
Chris R <cr625@drexel.edu>
2026-03-01 12:59:36 -0500

    clean up repo

--- nspe-pipeline/scrapers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-NSPE Case Scrapers
-----------------
-Modules for scraping and extracting case content from the NSPE website.
-"""
--- nspe-pipeline/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-NSPE Pipeline Utilities
----------------------
-Helper modules and database operations for the NSPE case processing pipeline.
-"""
